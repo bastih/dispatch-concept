156a9587e1f34fec2d1d603829d4a9060b377415
bastih <sebastian.hillig@gmail.com>
2013-08-06 12:34:16 +0000

    Docs

--- dispatch.cpp
+++ dispatch.cpp
@@ -11,6 +11,7 @@
 
 #include "cartesian.hpp"
 #include "strong_typedef.hpp"
+#include "debug.hpp"
 
 class ATable;
 class ADictionary;
@@ -88,23 +89,42 @@ class ATable : public Typed {};
 class Table : public BaseType<ATable, Table, table_types> {};
 class RawTable : public BaseType<ATable, RawTable, table_types> {};
 
-  
 class Operator {
+  // Operator defines special overloads for different combinations of
+  // ATable, AStorage, ADictionary descendants, so we can write
+  // special implementations
  public:
-  void execute_special(Table*, BitStorage*, OrderedDictionary*) {
-    std::cout << "Special for Table*, BitStorage*, OrderedDictionary*" << std::endl;
+  
+  void execute_special(Table*, BitStorage* bs, OrderedDictionary*) {
+    debug("Special for Table*, BitStorage*, OrderedDictionary*");
+    bs->BitStorage::get(1); // Not a virtual function call
   }
 
-  void execute_special(Table*, FixedStorage*, OrderedDictionary*) {
-    std::cout << "Special for Table*, FixedStorage*, OrderedDictionary*" << std::endl;
+  void execute_special(Table*, FixedStorage* fs, OrderedDictionary*) {
+    debug("Special for Table*, FixedStorage*, OrderedDictionary*");
+    fs->FixedStorage::getRef(1); // not a virtual function call
   }
 
   void execute_special(Table*, FixedStorage*, UnorderedDictionary*) {
-    std::cout << "Special for Table*, FixedStorage*, UnorderedDictionary*" << std::endl;
+    debug("Special for Table*, FixedStorage*, UnorderedDictionary*");
+  }
+
+  template<class ATABLE,
+           class ASTORAGE,
+           class ADICT,
+           /* protect from random invokations withouth at least being
+              part of the inheritance chain */
+           typename std::enable_if<std::is_base_of<ATable, ATABLE>::value, int>::type = 0,
+           typename std::enable_if<std::is_base_of<AStorage, ASTORAGE>::value, int>::type = 0,
+           typename std::enable_if<std::is_base_of<ADictionary, ADICT>::value, int>::type = 0>
+  void execute_general(ATABLE*, ASTORAGE* as, ADICT*) {
+    debug("General implementation kicks in");
+    as->ASTORAGE::get(0); // no virtual function call, uses actual types
   }
   
-  void execute_fallback(ATable*, AStorage*, ADictionary*) {
-    
+  void execute_fallback(ATable*, AStorage* as, ADictionary*) {
+    debug("Fallback kicks in");
+    as->get(0);
   }
 
   // Dispatch method
@@ -122,6 +142,11 @@ class HasExecuteSpecial {
   static const bool value = decltype(test<T>(0))::value;
 };
 
+template <typename T, typename S, typename D>
+bool matchingTypeIds(ATable* t, AStorage* s, ADictionary* d) {
+  return (t->getTypeId() == T::typeId) && (s->getTypeId() == S::typeId) && (d->getTypeId() == D::typeId);
+}
+
 // Call specialized implementation when available
 // Just slightly ugly: enable_if is valid when: op.execute_special(...) is overloaded exactly for tall params
 // through this specialization, we don't need a fallback execute_special for abstract base classes
@@ -131,13 +156,11 @@ template <class OP,
           class DICT,
           /* restricting unnamed parameter */
           typename std::enable_if<HasExecuteSpecial<OP, void, TABLE*, STORAGE*, DICT*>::value, int>::type = 0>
-void call_impl(OP& op, TABLE* table, STORAGE* store, DICT* dict) {
+void call_special(OP& op, TABLE* table, STORAGE* store, DICT* dict) {
   /// extracting table/store/dict actual typeIds through virtual function calls
   /// and compare to what we need for thise combination of types
-  const auto types = std::make_tuple(table->getTypeId(), store->getTypeId(), dict->getTypeId());
-  const auto expected_types = std::make_tuple(TABLE::typeId, STORAGE::typeId, DICT::typeId);
-  if (types == expected_types) {
-    std::cout << "Matching special found, executing" << std::endl;
+  if (matchingTypeIds<TABLE, STORAGE, DICT>(table, store, dict)) {
+    debug("Matching special found, executing");
     op.execute_special(table, store, dict);
   }
 }
@@ -149,10 +172,10 @@ template <class OP,
           typename std::enable_if<!HasExecuteSpecial<OP, void, TABLE*, STORAGE*, DICT*>::value, int>::type = 0>
 // Is valid when there is no viable overload in op for the given
 // params -- don't do anything, there is no match here
-void call_impl(OP& op, TABLE* table, STORAGE* store, DICT* dict) {}
+void call_special(OP& op, TABLE* table, STORAGE* store, DICT* dict) {}
 
 template <class OP>
-struct choose_impl {
+struct choose_special {
   OP& op;
   ATable* table;
   AStorage* store;
@@ -165,28 +188,46 @@ struct choose_impl {
     using DICT = typename boost::mpl::at_c<SEQUENCE, 2>::type;
     // cast all operators to their presumed types, so we can invoke
     // the correct implementation
-    call_impl(op, static_cast<TABLE*>(table), static_cast<STORAGE*>(store), static_cast<DICT*>(dict));
+    call_special(op, static_cast<TABLE*>(table), static_cast<STORAGE*>(store), static_cast<DICT*>(dict));
   }
 };
+
+
+template <class OP>
+struct general_impl {
+  OP& op;
+  ATable* table;
+  AStorage* store;
+  ADictionary * dict;
+
+  template <typename SEQUENCE>
+  inline void operator()() {
+    using TABLE = typename boost::mpl::at_c<SEQUENCE, 0>::type;
+    using STORAGE = typename boost::mpl::at_c<SEQUENCE, 1>::type;
+    using DICT = typename boost::mpl::at_c<SEQUENCE, 2>::type;
+    // cast all operators to their presumed types, so we can invoke
+    // the correct implementation
+    if (matchingTypeIds<TABLE, STORAGE, DICT>(table, store, dict)) {
+      op.execute_general(static_cast<TABLE*>(table), static_cast<STORAGE*>(store), static_cast<DICT*>(dict));
+    }
+  }
+};
+
 
 typedef boost::mpl::vector<table_types, storage_types, dictionary_types> types;
 
 void Operator::execute(ATable* tab, AStorage* store, ADictionary* dict) {
-  choose_impl<Operator> ci { *this, tab, store, dict };
+  choose_special<Operator> ci { *this, tab, store, dict };
   // generates the cartesian product of all types and per
   // combination SEQUENCE, invokes choose_impl<SEQUENCE>()
   boost::mpl::cartesian_product<types>(ci);
-}
 
-template<typename T>
-void debug(T arg) {
-  std::cout << arg << std::endl;
-}
+  // todo: if one impl was found, don't continue with general impl
+  general_impl<Operator> gi { *this, tab, store, dict };
+  boost::mpl::cartesian_product<types>(gi);
 
-template <typename T, typename... ARGS>
-void debug(T arg, ARGS... args) {
-  std::cout << arg << " ";
-  debug(args...);
+  // todo: if no impl was found, ie types were not registered
+  execute_fallback(tab, store, dict);
 }
 
 int main (int argc, char const *argv[]) {  
